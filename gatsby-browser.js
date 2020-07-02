0bfde490a610bd5bf3a92ca8f94beeb5dfb20a3c
Will Luft <william.d.luft@gmail.com>
2020-07-02 13:40:48 -0400

    Added Fonts and global stylesheet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import 'typeface-muli';
+import 'typeface-ovo';
+import './src/styles/global.css';
