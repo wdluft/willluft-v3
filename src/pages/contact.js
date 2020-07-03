062fde15a1c47a9d0f64529d3d033f39767ed164
Will Luft <william.d.luft@gmail.com>
2020-07-03 19:23:07 -0400

    Created about and contact page routes

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <div>
+      <h1>Contact</h1>
+      <p>@IAmWillDL</p>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
