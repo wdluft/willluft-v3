5538b17d435e96be34579a4f665f779b151401d7
Will Luft <william.d.luft@gmail.com>
2020-07-11 22:08:52 -0400

    Added about info to index/home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,46 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
+import { TransparentCard } from '../styles/elements';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div>
+    <TransparentCard>
       <h3>Hi, I'm</h3>
       <h1>WILL LUFT</h1>
-      <p>I build websites, do customer support, and play sports.</p>
-    </div>
+      <p>
+        I'm a Customer Support Specialist for{' '}
+        <a
+          href="https://www.donorschoose.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DonorsChoose
+        </a>
+        , helping teachers and donors with any questions they have.
+      </p>
+      <p>
+        I am an aspiring web developer, focusing on HTML, CSS, JavaScript, and
+        React. Below are some of the projects I've built.
+      </p>
+      <p>
+        To get my thoughts out of my head, I write for my blog,{' '}
+        <a
+          href="https://www.becomebydoing.me"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Become by Doing
+        </a>
+        . I'll write about anytihng ranging from web development, to what's
+        going on in the world, to my personal philosophy.
+      </p>
+      <p>
+        Please <Link to="/contact">reach out to me</Link> if you'd like to chat
+        or build something together.
+      </p>
+    </TransparentCard>
   </Layout>
 );
 
