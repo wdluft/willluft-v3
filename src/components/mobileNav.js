e84353e615337e90edf32508e5bbde57df31f765
Will Luft <william.d.luft@gmail.com>
2020-07-27 17:25:24 -0400

    Added menu close to mobile nav when link is clicked

--- src/components/mobileNav.js
+++ src/components/mobileNav.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { NavWrapper } from '../styles/elements';
 import { useScrollFreeze } from '../hooks';
+import { useAppState } from '../state';
 
 const MobileNav = () => {
   useScrollFreeze();
+  const { toggleMenu, isMenuOpen } = useAppState();
 
   return (
     <NavWrapper>
       <div className="siteNav">
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/resume">Resume</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/" onClick={toggleMenu}>
+          Home
+        </Link>
+        <Link to="/projects" onClick={toggleMenu}>
+          Projects
+        </Link>
+        <Link to="/resume" onClick={toggleMenu}>
+          Resume
+        </Link>
+        <Link to="/contact" onClick={toggleMenu}>
+          Contact
+        </Link>
       </div>
     </NavWrapper>
   );
